bde4eb997801868aa97ea012acff8ef1b272007f
bcgosselin <bcgosselin@gmail.com>
2024-05-14 15:22:13 -0500

    added seeds

---
+++ seeds/department_seed.sql
@@ -0,0 +1,7 @@
+USE employeetracker_db;
+
+INSERT INTO departments (name) VALUES
+('Sales'),
+('Engineering'),
+('Finance'),
+('Legal');
---
+++ seeds/roles_seed.sql
@@ -0,0 +1,11 @@
+USE employeetracker_db;
+
+INSERT INTO roles (id, title, salary, department_id) VALUES
+(1, 'Sales Lead', 100000, 1),
+(2, 'Sales Person', 80000, 1),
+(3, 'Lead Engineer', 150000, 2),
+(4, 'Software Engineer', 120000, 2),
+(5, 'Account Manager', 160000, 3),
+(6, 'Accountant', 125000, 3),
+(7, 'Legal Team Lead', 250000, 4),
+(8, 'Lawyer', 190000, 4);
